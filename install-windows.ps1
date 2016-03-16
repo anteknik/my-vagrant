00b77184893c80d559f3420cc5e1aa77e05b0c39
anteknik <anton.takaful@gmail.com>
2016-03-16 08:33:00 +0700

    vagrant nginx web-dev

--- install-windows.ps1
+++
@@ -1,7 +0,0 @@
-# make sure you run this as administrator
-
-# install chocolatey
-iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-
-# install vagrant and virtualbox
-cinst vagrant virtualbox
